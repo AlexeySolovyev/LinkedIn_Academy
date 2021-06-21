87409100a38ad2b6b8d9dc2a27c7d1239ea98242
Alexey Solovyev <solovevalex@gmail.com>
2021-06-21 19:42:24 -0400

    Autocomplete Test Case has been fixed

--- src/test/java/Autocomplete.java
+++ src/test/java/Autocomplete.java
@@ -11,36 +11,12 @@ public class Autocomplete {
 
         driver.get("https://formy-project.herokuapp.com/autocomplete");
 
-        WebElement mainAddress = driver.findElement(By.id("autocomplete"));
-        WebElement streetAddress = driver.findElement(By.id("street_number"));
-        WebElement streetAddress2 = driver.findElement(By.id("route"));
-        WebElement city = driver.findElement(By.id("locality"));
-        WebElement state = driver.findElement(By.id("administrative_area_level_1"));
-        WebElement zip = driver.findElement(By.id("postal_code"));
-        WebElement country = driver.findElement(By.id("country"));
-
-        mainAddress.click();
-        mainAddress.clear();
-        mainAddress.sendKeys("24 - 1");
-        streetAddress.click();
-        streetAddress.clear();
-        streetAddress.sendKeys("Third Avenue");
-        streetAddress2.click();
-        streetAddress2.clear();
-        streetAddress2.sendKeys("private front house");
-        city.click();
-        city.clear();
-        city.sendKeys("New York");
-        state.click();
-        state.clear();
-        state.sendKeys("New York");
-        zip.click();
-        zip.clear();
-        zip.sendKeys("11021");
-        country.click();
-        country.clear();
-        country.sendKeys("USA");
-        Thread.sleep(3000);
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1555 Ocean Parkway, Brooklyn, NY, USA");
+        Thread.sleep(1000);
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+        Thread.sleep(5000);
         driver.quit();
     }
 }

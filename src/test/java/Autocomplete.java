c8c36182943d65b2fe0128ffc184d7cd5c6de3f7
Alexey Solovyev <solovevalex@gmail.com>
2021-06-22 16:20:25 -0400

    Fix Autocomplete Test Case by adding ExplicitlyWait function

--- src/test/java/Autocomplete.java
+++ src/test/java/Autocomplete.java
@@ -2,10 +2,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class Autocomplete {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/opt/WebDriver/bin/chromedriver");
         WebDriver driver = new ChromeDriver();
 
@@ -13,10 +18,14 @@ public class Autocomplete {
 
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
         autocomplete.sendKeys("1555 Ocean Parkway, Brooklyn, NY, USA");
-        Thread.sleep(1000);
-        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        //Thread.sleep(5000);
+        // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // change Thread.sleep by ImlicitlyWait
+        //WebElement autocompleteResult = driver.findElement(By.className("pac-item")); // change implicitlyWait by explicitlyWait
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement autocompleteResult = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pac-item")));
         autocompleteResult.click();
-        Thread.sleep(5000);
+
         driver.quit();
     }
 }

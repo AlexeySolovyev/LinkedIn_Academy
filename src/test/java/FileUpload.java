8cebebad413e94a2676b808d7583995bf601ba38
Alexey Solovyev <solovevalex@gmail.com>
2021-06-22 15:39:27 -0400

    Test case for File Upload feature.

--- src/test/java/FileUpload.java
+++ src/test/java/FileUpload.java
@@ -1,3 +1,20 @@
-public class FileUpload {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class FileUpload {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/opt/WebDriver/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/fileupload");
+        // WebElement chooseButton = driver.findElement(By.cssSelector(".btn.btn-secondary.btn-choose"));
+        // WebElement resetButton = driver.findElement(By.cssSelector(".btn.btn-warning.btn-reset"));
+        WebElement fileUploadField = driver.findElement(By.id("file-upload-field"));
+        fileUploadField.sendKeys("src/test/files/Contracts.png");
+
+        driver.quit();
+    }
 }
